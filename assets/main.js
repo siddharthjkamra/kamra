e7b72ea337620e3b84fdad3d5b0d5fb13e2d7056
Siddharth Kamra <sidhrthkmr@gmail.com>
2022-06-28 09:08:04 +0530

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -25,3 +25,13 @@ function includeHTML() {
     }
   }
 };
+
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+};
